58d48ad61ca51846b070c10983c8c7c163811779
Hezkiel Rajani Aritonang_123140118 <83205393+Rendom-user@users.noreply.github.com>
2025-06-01 18:37:52 +0700

    Add files via upload

---
+++ tubes1-IF2211-bot-starter-pack-1.0.1/a/game/run-bots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python main.py --logic casualbot --email=test@email.com --name=ardi --password=222222 --team etimot &
+python main.py --logic botgaklogis --email=test1@email.com --name=maskiel --password=111111 --team etimob &
+python main.py --logic gacorbot --email=test2@email.com --name=cipa --password=333333 --team etimom &
+python main.py --logic superbot --email=test3@email.com --name=gelo --password=444444 --team etimoc &
